77c202bf1daa900f36acfe2d8d31faeecd1f479a
Juho Park <todarii@naver.com>
2022-09-30 22:58:10 +0900

    .

--- src/exercise/exercise6_2.java
+++ src/exercise/exercise6_2.java
@@ -10,18 +10,19 @@ package exercise;
 
 public class exercise6_2 {
 		public static void main(String args[]) {
-			SutdaCard card1 = new SutdaCard(3, false);
-			SutdaCard card2 = new SutdaCard(); 
-			System.out.println(card1.info());
-			System.out.println(card2.info());
+//			SutdaCard card1 = new SutdaCard(3, false);
+//			SutdaCard card2 = new SutdaCard(); 
+//			System.out.println(card1.info());
+//			System.out.println(card2.info());
+//		}
+//}
+//		class SutdaCard {
+//					
+//			int num; 
+//			boolean isKwang;
+//			
+//	
+			
 		}
 }
-		class SutdaCard {
-					
-			int num; 
-			boolean isKwang;
-			
-	
-			
-		}
 
